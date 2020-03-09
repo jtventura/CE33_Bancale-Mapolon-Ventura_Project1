b2cfd19d47ca898e56753093f9694296a330eae2
unknown <christian.bancale@obf.ateneo.edu>
2020-03-09 16:51:03 +0800

    start bairstow

--- code/roots.cpp
+++ code/roots.cpp
@@ -31,18 +31,20 @@ int main(int argc, char *argv[])
     print_polynomial(coefficients);
     cout << endl;
     
-    // test for p2.txt
-    vector<complex<double>> roots;
-    complex<double> r1(2,0);
-    complex<double> r2(-1,0);
-    complex<double> r3(1,0.5);
-    complex<double> r4(1,-0.5);
-    complex<double> r5(0.5,0);
-    roots.push_back(r1);
-    roots.push_back(r2);
-    roots.push_back(r3);
-    roots.push_back(r4);
-    roots.push_back(r5);
+    vector<complex<double>> roots = bairstow_method(coefficients);
+    // complex<double> r1(2,0);
+    // complex<double> r2(-1,0);
+    // complex<double> r3(1,0.5);
+    // complex<double> r4(1,-0.5);
+    // complex<double> r5(0.5,0);
+    // roots.push_back(r1);
+    // roots.push_back(r2);
+    // roots.push_back(r3);
+    // roots.push_back(r4);
+    // roots.push_back(r5);
+    cout << "Roots:" << endl;
+    // print_roots(roots);
+    // cout << endl;
 
     cout << "Evaluating polynomial at identified roots:" << endl;
     for (auto root: roots)
@@ -88,13 +90,16 @@ vector<double> get_coefficients(string fileName)
     return coefficients;
 }
 
-// vector<complex<double>> bairstow_method(vector<double> coefficients)
-// {
-//     double r = 0.5;
-//     double s = -0.5;
-//     double e = 0.01;
+vector<complex<double>> bairstow_method(vector<double> coefficients)
+{
+    int deg = coefficients.size();
+    int n = coefficients.size() - 1;
+    double r = -coefficients[n-1]/coefficients[n];
+    double s = -coefficients[n-2]/coefficients[n];
+    double e = 0.01;
 
-// }
+    
+}
 
 void print_polynomial(vector<double> coefficients)
 {
@@ -104,7 +109,8 @@ void print_polynomial(vector<double> coefficients)
 
 void print_roots(vector<complex<double>> roots)
 {
-    //
+    for (auto root: roots)
+        cout << root << endl;
 }
 
 void horner_method(vector<double> coefficients, complex<double> root)
